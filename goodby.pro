f994a2a65e2243ad01cf87163b2a794f4ff9917a
Gary B. Corell <wa5qjh@xmission.com>
2017-04-26 17:21:09 +0800

    Add files via upload

---
+++ goodby.pro
@@ -0,0 +1,7 @@
+# create a makefile for hello using make
+#  qmake -o Makefile hello.c
+
+	TARGET = goodby
+#	TEMPLATE = app
+
+	SOURCES += hello.c
---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+
+int main()
+{
+/*  char kill ="shutdown -r now";   */
+ printf("help!!");
+	system(" ls -lai  *.c");
+/*	system("shutdown -p now");     */
+}
